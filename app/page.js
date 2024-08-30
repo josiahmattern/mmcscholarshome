4bde73d1f20b466c2343600ed2afa463a9a187cc
Josiah Mattern <josiahleemattern@gmail.com>
2024-08-30 09:02:09 -0400

    removed navbar import on main page

--- app/page.js
+++ app/page.js
@@ -1,11 +1,9 @@
-import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import Schedule from "../components/Schedule";
 
 export default function Home() {
   return (
     <main>
-
       <Hero />
       <Schedule id="schedule" isAdmin={false} />
     </main>
