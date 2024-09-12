29fa6e507f38fd8532927ca26c587c1bdc6822f5
Josiah Mattern <josiahleemattern@gmail.com>
2024-09-12 10:40:55 -0400

    added use client to leaderboard

--- components/StudentLeaderboard.js
+++ components/StudentLeaderboard.js
@@ -1,3 +1,4 @@
+"use client";
 import React, { useEffect, useState, useRef } from "react";
 import { ref, onValue, push, set, update, remove } from "firebase/database";
 import { db } from "@/lib/firebaseConfig.js";
