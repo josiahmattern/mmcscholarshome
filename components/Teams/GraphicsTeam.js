0ce5e6ed65b1ce52d5d6dd16b90434f5b166be67
passionfirex <126196790+passionfirex@users.noreply.github.com>
2024-09-16 11:48:49 -0400

    added blurbs for each team

--- components/Teams/GraphicsTeam.js
+++ components/Teams/GraphicsTeam.js
@@ -1,9 +1,16 @@
 
 export default function GraphicsTeam() {
   return (
-  <main>
-      <div>
-       Graphics team 
+    <main>
+      <div className="px-4 w-full text">
+        <p className="text-lg">
+          The MMC Digital Lab Graphics Team creates digital and physical media for the MMC Digital Lab and MMC Scholars program.
+          Student employees are trained in multiple programs within the Adobe Suite, including Adobe Illustrator, Photoshop,
+          and InDesign. With this expertise, Graphics Team members learn how to create physical items such as stickers, buttons,
+          and t-shirts with various tools (Button Maker Machine, Cricut Explore 3, etc.). Additionally, Graphics team members are
+          highly encouraged to explore their interests and expand their skills by learning additional creative software and
+          techniques (e.g., motion graphics, infographics, design for social media).
+        </p>
       </div>
     </main>
   );
--- components/Teams/WebDevTeam.js
+++ components/Teams/WebDevTeam.js
@@ -1,8 +1,14 @@
 export default function WebDevTeam() {
   return (
-  <main>
-      <div>
-       Web dev 
+    <main>
+      <div className="px-4 w-full text">
+        <p className="text-lg">
+          The MMC Digital Lab Web Dev Team develops websites and materials for the MMC Digital Lab and the MMC Scholars program.
+          Student employees are trained in many coding frameworks, including JavaScript, HTML/CSS, Next.js, and React; additionally,
+          student employees train and build sites within the content management system Drupal. Web Dev team members are encouraged
+          to explore their interests and expand their skillsets by training in other development software such as Blender, Unity,
+          and Python.
+        </p>
       </div>
     </main>
   );
